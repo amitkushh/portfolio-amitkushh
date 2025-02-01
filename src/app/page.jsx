11b369afc7f0e17e766b610345866bde1466a30a
Amit Kushwaha <amitcoding56@gmail.com>
2025-02-01 16:18:38 +0530

    drawer added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,10 +3,12 @@ import About from "@/components/About";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import Footer from "@/components/Footer";
+import Drawer from "../components/Common/Drawer";
 
 export default function Home() {
   return (
     <>
+      <Drawer />
       <Header />
       <About />
       <Projects />
